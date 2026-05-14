1854a89a4221cf7f53265dd90a783e6004f6877e
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-05-14 00:16:30 -0300

    cambios ahora en el css de details-bar

--- src/components/Details-bar.jsx
+++ src/components/Details-bar.jsx
@@ -1,16 +1,13 @@
 import { useParams } from 'react-router-dom';
 import "./details-bar.css"
-
-
-
 export function Details_bar({ moneda }) { 
     // Si moneda no llega (por si acaso), evitamos que explote
     if (!moneda) return null; 
 
     return (
-        <div className="details-bar">
+        <div className="Details-bar">
             <h1>{moneda.name}</h1>
-            <p>Precio: {moneda.price_usd}</p>
+            <p>Precio: {moneda.precio}</p>
         </div>
     );
 }
